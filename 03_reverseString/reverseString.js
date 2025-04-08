2c42b22a9d9b33949d60afc275c4cc989e61a0a5
ZBO-R8io <parkman.dustin550@gmail.com>
2025-04-08 13:19:36 -0500

    Completed exercise #03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    array = Array.from(str);
+    reverseArray = array.reverse();
+    return(reverseArray.join(''));;
 };
+
+reverseString('hello');
+
+reverseString('hello there');
+
+reverseString('123! abc! Hello, Odinite.');
+
+reverseString('');
 
 // Do not edit below this line
 module.exports = reverseString;
